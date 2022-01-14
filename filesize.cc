aaef8e88dca7dca6447ed2ef406a3b79b7f88d03
suzya-ctrl <54541104+suzya-ctrl@users.noreply.github.com>
2022-01-14 14:24:55 -0500

    Final Commit with Comments

--- filesize.cc
+++ filesize.cc
@@ -1,28 +1,34 @@
+/*******************************************************************
+ * Name: Suzan Abdelqader
+ * Class: CS 4420
+ * Professor: Professor Osterman
+ * Program: Filesize.cc
+ * Description: This program will take in a file(s) and output the amount of lines in each file.
+ ********************************************************************/
 #include <iostream>
 #include <fstream>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    ifstream filename;
-     string line;
-        cout << "program: " << argv[0] << endl;
+    ifstream filename; //name for file 
+     string line; //holds a line in the file
+        cout << "program: " << argv[0] << endl; //prints to console name of program
     // just to get you started, this is how to refer to the arguments that were passed
-    for (int arg = 1; arg < argc; arg++){
-                int counter = 0;
-        filename.open(argv[arg]);
+    for (int arg = 1; arg < argc; arg++){ //increments the amount of arguements to get through all the files if there is more than 1
+                int counter = 0; //represents amount of lines in the file
+        filename.open(argv[arg]); //opens the file 
         
-        if(filename.fail()){
-            cout << " " <<  argv[arg] << ": -1" << endl; 
+        if(filename.fail()){ // checks if file has failed to open/unreadable
+            cout << " " <<  argv[arg] << ": -1" << endl; //if the file doesnt open a -1 is outputted to the console
         } else{
         
-        while(getline(filename, line)){
-            counter++;
+        while(getline(filename, line)){ //if the file is openable goes through each line
+            counter++; //every line that is found the counter is incremented
     }
-        cout << " " << argv[arg] << ": " << counter << endl;
-        filename.close();
+        cout << " " << argv[arg] << ": " << counter << endl; // prints amoutn of lines in each file to the screen
+        filename.close(); //closes the file
     }
     }
     exit(0); // this means that the program executed correctly!
 }
-
